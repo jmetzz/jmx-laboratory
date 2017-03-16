c9bf433bb592b6bc02aa888f1d0135910a2b8d1b
Jean Metz <jean.metz@ravago.com>
2017-03-16 17:07:05 +0100

    Add JMX configuration to spring context

--- src/main/java/com/github/jmetzz/laboratory/JmxPocApplication.java
+++ src/main/java/com/github/jmetzz/laboratory/JmxPocApplication.java
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JmxPocApplication {
 
+	/*
+	 All objects defined as Spring bean by means of spring annotations will only be pick up
+	 when their respective package is included in a component scan.
+	 A default behavior of Spring Boot when using the @SpringBootApplication annotation is
+	 to perform a package scan on its package (and all sub packages).
+	 Because the application class (this one) resides in the package com.github.jmetzz.laboratory,
+	 the annotated Spring Components in the child packages are automatically found by Spring.
+	 If they were in a different package tree, I would need to explicitly tell Spring Boot to scan that package.
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(JmxPocApplication.class, args);
 	}
--- src/main/java/com/github/jmetzz/laboratory/controllers/IndexController.java
+++ src/main/java/com/github/jmetzz/laboratory/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.github.jmetzz.laboratory.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,13 +15,18 @@ import com.github.jmetzz.laboratory.helpers.AServiceClass;
 public class IndexController {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private AServiceClass aService;
+
     @RequestMapping("/")
     public String index(){
         LOGGER.debug("This is a debug message");
         LOGGER.info("This is a info message");
         LOGGER.warn("This is a warning message");
         LOGGER.error("This is a error message");
-        new AServiceClass().aMethod();
+
+        aService.aMethod();
+
         return "index";
     }
 }
